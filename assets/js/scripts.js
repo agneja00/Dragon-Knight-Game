9599661109446fe361a89d46e2d118113ad6666b
agneja00 <agnieshaa00@gmail.com>
2025-10-11 22:54:15 +0300

    Import and call checkIfEndOfGame after each round in scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,19 @@
 import { elements } from "./elements.js";
 import { roundType } from "./constants.js";
 import playRound from "./playRound.js";
+import checkIfEndOfGame from "./endGame.js";
 
 elements.attackButton.addEventListener("click", function () {
   playRound(roundType.attack);
+  checkIfEndOfGame();
 });
 
 elements.defendButton.addEventListener("click", function () {
   playRound(roundType.defend);
+  checkIfEndOfGame();
 });
 
 elements.healButton.addEventListener("click", function () {
   playRound(roundType.heal);
+  checkIfEndOfGame();
 });
